2d8cab1f8b006adb6dff2ecdd762460e1490fbfb
Niko Paasiniemi <niko.paasiniemi@helsinki.fi>
2022-10-09 14:14:06 +0300

    fix typo

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 import re
 
+# heroku provides the url in the wrong format
+def fix_pg_url(input: str) -> str:
+    return re.sub("^postgres:", "postgresql:", input)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = fix_pg_url(getenv("DATABASE_URL"))
 db = SQLAlchemy(app)
-
-# heroku provides the url in the wrong format
-def fix_pg_url(input: str) -> str:
-    return re.sub("^postgres:", "postgresql:", input)
 
 def get_albums(db) -> list:
     result = db.session.execute("""
